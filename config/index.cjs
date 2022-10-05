50d6b26430b833615dd8672e0e54ff7d082f94c0
linhuangwei <42707526+linhuangwei@users.noreply.github.com>
2022-10-05 12:17:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,7 +54,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o16I-5u_CnrQFPyy1nLIGutiNeis',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o_z_fxxbfnZKkyh6oaH7qKeaIYfLIRhrmeyLd3cJlQM',
+      useTemplateId: 'OcxKTiSGRIWYmmO_mtAprlHySHoM7TRvWuCQzU4LzOw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-17',
       openUrl: 'https://linhuangwei.github.io',
@@ -85,7 +85,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o16I-5sl4WpPgEcKgzJGwFPxDzUg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o_z_fxxbfnZKkyh6oaH7qKeaIYfLIRhrmeyLd3cJlQM',
+      useTemplateId: 'OcxKTiSGRIWYmmO_mtAprlHySHoM7TRvWuCQzU4LzOw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-17',
       openUrl: 'https://linhuangwei.github.io',
