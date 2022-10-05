659757fbf0d7b0977690e5985a76e96efd7fe5b4
linhuangwei <42707526+linhuangwei@users.noreply.github.com>
2022-10-05 12:31:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -85,7 +85,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o16I-5sl4WpPgEcKgzJGwFPxDzUg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'G6jwkgIRFABWZt2Y_clCjS-J65nwZG10Tsb0ZRBjSVE',
+      useTemplateId: 'ggXPY38ljnauOG-m092YdTKPqxaMYvAJ6KBUzGmN2KQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-17',
       openUrl: 'https://linhuangwei.github.io',
